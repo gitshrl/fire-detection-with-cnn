040799dc0dee9e6cbb17b16e4385bb97e972bd02
hssahrul <sahrulhs.sukardi@gmail.com>
2019-12-17 04:45:06 +0700

    add demo kit

---
+++ Demo/detection.py
@@ -0,0 +1,51 @@
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import img_to_array
+import numpy as np
+import cv2
+import time
+
+model = load_model('Fire-64x64-CNN-v2.h5')
+
+cap = cv2.VideoCapture('NewProject.mp4')
+time.sleep(2)
+
+if cap.isOpened():
+    rval, frame = cap.read()
+else:
+    rval = False
+
+IMG_SIZE = 64
+
+while True:
+    rval, image = cap.read()
+    
+    if rval == True:
+        orig = image.copy()
+
+        image = cv2.resize(image, (IMG_SIZE, IMG_SIZE))  
+        image = image.astype("float") / 255.0
+        image = img_to_array(image)
+        image = np.expand_dims(image, axis=0)
+        
+        fire_prob = model.predict(image)[0][0]
+        label = "Confidence rate: {:.2f}".format(fire_prob)
+        
+        if fire_prob > 0.50:
+            color = (0, 0, 255)
+        else:
+            color = (0, 255, 0)
+        
+        cv2.putText(orig, label, (10, 25),  cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+
+        cv2.imshow("Output", orig)
+        
+        key = cv2.waitKey(10)
+        if key == 27:
+            break
+    elif rval==False:
+            break
+
+cap.release()
+cv2.destroyAllWindows()
+
